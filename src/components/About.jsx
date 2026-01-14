5a397f8ce342fbd15ed1613f79385adcd2df139d
Mikeyor97 <No user email found>
2026-01-14 20:32:53 +0200

    14.1 fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,11 @@ function About() {
         <div>
         <div className="about-flex-wrapper">
         <div className="about-flex-container">
-        <h1>Greetings!</h1>
-        <br/>
-        <p>I'm Henri. I am currently a student in programming education in Riveria.</p>
-        <p>I am learning website programming and designing. I may become an entrepreneur in the future.</p>
-        <p>I am interested in arts, crafts, fashion and design. I aspire to make great work of art someday.</p>
+        <div className="info-box info-box-1"></div>
+        <div className="info-box info-box-2"></div>
+        <div className="info-box info-box-3"></div>
+        <div className="info-box info-box-4"></div>
+        <div className="info-box info-box-5"></div>
         </div>
         </div>
         </div>
