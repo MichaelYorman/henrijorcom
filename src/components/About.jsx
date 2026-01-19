3f53ef8fbc4af52c48db5a9f6a09fd73ed2252cc
Mikeyor97 <No user email found>
2026-01-19 22:10:48 +0200

    devaus jatkuu

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,33 +20,38 @@ function About() {
         </div>
         <div className="about-flex-container">
         <div className='info-box-container info-box-container-1'>
-        <div className="info-box-header info-box-header-1">Who I am?</div>
+        <div className="info-box-header info-box-header-1">What I do?</div>
         <div className="info-box info-box-1">
-        <img className="info-box-image info-box-image-1" img src="/icons/about/user.svg" alt="User logo"></img>
+        <img className="info-box-image info-box-image-1" img src="/icons/user.svg" alt="Gear logo"></img>
         </div>
         </div>
         <div className='info-box-container info-box-container-2'>
         <div className="info-box-header info-box-header-2">What I do?</div>
         <div className="info-box info-box-2">
-        <img className="info-box-image info-box-image-1" img src="/icons/about/gear.svg" alt="Gear logo"></img>
+        <img className="info-box-image info-box-image-2" img src="/icons/gear.svg" alt="Gear logo"></img>
         </div>
         </div>
         <div className='info-box-container info-box-container-3'>
         <div className="info-box-header info-box-header-3">What do I know?</div>
         <div className="info-box info-box-3">
-        <img className="info-box-image info-box-image-1" img src="/icons/about/book.svg" alt="Book logo"></img>
+        <img className="info-box-image info-box-image-3" img src="/icons/book.svg" alt="Book logo"></img>
+        <img className="info-box-skill-icon info-box-skill-icon-1" img src="icons/skills/css3.svg" alt="CSS3 logo"></img>
+        <img className="info-box-skill-icon info-box-skill-icon-1" img src="icons/skills/javascript.svg" alt="Javascript logo"></img>
+        <img className="info-box-skill-icon info-box-skill-icon-1" img src="icons/skills/mysql.svg" alt="Mysql logo"></img>
+        <img className="info-box-skill-icon info-box-skill-icon-1" img src="icons/skills/python.svg" alt="Python logo"></img>
+        <img className="info-box-skill-icon info-box-skill-icon-1" img src="icons/skills/react.svg" alt="React logo"></img>
         </div>
         </div>
         <div className='info-box-container info-box-container-4'>
         <div className="info-box-header info-box-header-4">What I want to do?</div>
         <div className="info-box info-box-4">
-            <img className="info-box-image info-box-image-1" img src="/icons/about/cloud.svg" alt="Cloud logo"></img>
+            <img className="info-box-image info-box-image-4" img src="/icons/cloud.svg" alt="Cloud logo"></img>
         </div>
         </div>
         <div className='info-box-container info-box-container-5'>
         <div className="info-box-header info-box-header-5">What are my interests?</div>
         <div className="info-box info-box-5">
-        <img className="info-box-image info-box-image-1" img src="/icons/about/heart.svg" alt="User logo"></img>
+        <img className="info-box-image info-box-image-5" img src="/icons/heart.svg" alt="User logo"></img>
         </div>
         </div>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,17 +11,17 @@ function Home() {
         <section className="home-grid-container">
         <NavLink to="/about">
         <div className="card card-1">
-       <img className="home-image" img src="/icons/about/home.svg" alt="Cottage logo"></img>
+       <img className="home-image" img src="/icons/user.svg" alt="User logo"></img>
         </div>
         </NavLink>
         <NavLink to="/resume">
         <div className="card card-2">
-        <img className="home-image" img src="/icons/resume.svg" alt="Cottage logo"></img> 
+        <img className="home-image" img src="/icons/resume.svg" alt="Paper and pen logo"></img> 
         </div>
         </NavLink>
         <NavLink to="/portfolio">
         <div className="card card-3">
-        <img className="home-image" img src="/icons/folder.svg" alt="Cottage logo"></img>
+        <img className="home-image" img src="/icons/folder.svg" alt="Folder logo"></img>
         </div>
         </NavLink>
         </section>
