e4682f2a72d1dcf7394dfc9d283ac5b3785ef43f
Mikeyor97 <No user email found>
2026-01-25 20:59:37 +0200

    added some placeholders

--- src/components/Portfolio/Preview.jsx
+++ src/components/Portfolio/Preview.jsx
@@ -7,6 +7,23 @@ function Preview() {
             <div className="portfolio-preview-box">
             <label>Portfolio Preview</label>
             <div className="portfolio-preview-content">
+            <div className="portfolio-preview-image"></div>
+            <div className="portfolio-preview-header">Project name</div>
+            <div className="portfolio-preview-text">a</div>
+            <div className="portfolio-preview-header">Role</div>
+            <div className="portfolio-preview-text">b</div>
+            <div className="portfolio-preview-header">Field</div>
+            <div className="portfolio-preview-text">c</div>
+            <div className="portfolio-preview-header">Description</div>
+            <div className="portfolio-preview-text">d</div>
+            <div className="portfolio-preview-header">Status</div>
+            <div className="portfolio-preview-text">e</div>
+            <div className="portfolio-preview-header">Time</div>
+            <div className="portfolio-preview-text">f</div>
+            <div className="portfolio-preview-header">Time</div>
+            <div className="portfolio-preview-text">f</div>
+            <div className="portfolio-preview-header">Time</div>
+            <div className="portfolio-preview-text">f</div>
             </div>
             </div>
         </div>
